f602fd5d37db09c5f6494887a5ea085b4ae48ec4
Faucon <gass.anthony@gmail.com>
2025-11-17 14:52:46 +1100

    feat(register): template register

--- front/src/router/AppRouter.jsx
+++ front/src/router/AppRouter.jsx
@@ -1,12 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home/Home.jsx";
 import DroneList from "../pages/DroneList/DroneList.jsx"
+import Connexion from "../pages/Connexion/Connexion.jsx";
 
 export default function AppRouter() {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/listProducts" element={<DroneList />} />
+            <Route path="/connexion" element={<Connexion />} />
+            
         </Routes>
     );
 }
