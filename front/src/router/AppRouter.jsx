2d8e9c4477bf4e7c8c2287072626d75f7aeb20d5
Florian242 <floriancata04@gmail.com>
2025-11-17 15:08:51 +1100

    ajouter panier

--- front/src/router/AppRouter.jsx
+++ front/src/router/AppRouter.jsx
@@ -1,12 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home/Home.jsx";
-import DroneList from "../pages/DroneList/DroneList.jsx"
+import DroneList from "../pages/DroneList/DroneList.jsx";
+
+
+import Panier from "../components/panier/panier.jsx";
 
 export default function AppRouter() {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/listProducts" element={<DroneList />} />
+            <Route path="/panier" element={<Panier />} />
         </Routes>
     );
 }
